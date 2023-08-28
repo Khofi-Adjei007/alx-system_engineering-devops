98790b836c4b3967cd5021b406805938b59da188
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-08-28 15:27:25 +0000

    1-repetition_token_0.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-# This script takes a command line argument and searches for occurrences of the pattern "[S]chool".
 #Auth: Khofi Adjei Kingsford
 puts ARGV[0].scan(/School/).join
